f1144bafc003d914b6660456f517ebff6b5e7bf1
CarmenHe <43501759+CarmenHe@users.noreply.github.com>
2024-03-07 14:29:55 -0800

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -1,11 +1,11 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
-
+# No set -e : we want to check for errors with this program
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+# if something goes wrong with git clone, only the students need to see this
+git clone $1 student-submission > ta-output.txt
 echo 'Finished cloning'
 
 
@@ -14,3 +14,38 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# check for missing file
+if [[ -f student-submission/$2.java ]]
+then
+    cp student-submission/$2.java grading-area
+    cp Test$2.java grading-area
+else
+    echo "Missing $2 file"
+    exit 1
+fi
+
+# set up testing in grading area
+cd grading-area
+
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
+javac -cp $CPATH *.java > compile-message.txt
+
+if [[ $? -ne 0 ]]
+then
+    echo "Compile error"
+    cat test-results.txt
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore Test$2 > test-results.txt
+
+# calculate score based on failures
+
+lastline = $(cat test-results.txt | tail -n 2 | head -n 1)
+tests = $(echo $lastline | awk -F'[, ]' '{print $3}')
+failures = $(echo $lastline | awk -F'[, ]' '{print $6}')
+successes = $(tests-failures)
+
+echo "Your score is $successes / $tests"
+
