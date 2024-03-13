8a4d967c9a5db0f87a338a801cb7410413c2b331
CarmenHe <43501759+CarmenHe@users.noreply.github.com>
2024-03-12 20:30:31 -0700

    completed grade server

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,4 +20,29 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+    public void testMergeDiffOrder(){
+        List<String> list1 = new ArrayList<>();
+        list1.add("a");
+        list1.add("c");
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("b");
+        list2.add("d");
+
+        assertArrayEquals(new String[] {"a","b","c","d"}, ListExamples.merge(list2,list1).toArray());
+    }
+
+   @Test(timeout = 500)
+    public void testFilter(){
+        StringChecker minLen5Checker = new StringChecker() {
+            public boolean checkString(String s){return s.length()>=5;}
+        };
+        List<String> input1 = new ArrayList<>();
+        input1.add("Hi");
+        input1.add("Hello");
+        input1.add("Good Day");
+        assertArrayEquals(new String[] {"Hello","Good Day"}, ListExamples.filter(input1, minLen5Checker).toArray());
+    }
 }
--- grade.sh
+++ grade.sh
@@ -4,8 +4,9 @@ rm -rf grading-area
 
 mkdir grading-area
 
-# if something goes wrong with git clone, only the students need to see this
-git clone $1 student-submission > ta-output.txt
+# if something goes wrong with git clone, only the tas need to see this
+# $1 = file the student submits
+git clone $1 student-submission
 echo 'Finished cloning'
 
 
@@ -16,36 +17,70 @@ echo 'Finished cloning'
 # tests
 
 # check for missing file
-if [[ -f student-submission/$2.java ]]
+# $2 = file to grade and check for
+# if [[ -f student-submission/$2.java ]]
+# then
+#     cp student-submission/$2.java grading-area
+#     cp Test$2.java grading-area
+# else
+#     echo "Missing ListExamples file"
+#     exit 1
+# fi
+# cd student-submission/
+if [[ $(find './student-submission/' -name 'ListExamples.java') ]]
 then
-    cp student-submission/$2.java grading-area
-    cp Test$2.java grading-area
+   cp -r student-submission/ grading-area
+   cp TestListExamples.java grading-area
 else
-    echo "Missing $2 file"
-    exit 1
+   echo "Missing ListExamples file. Make sure you submit files with the matching name."
+   exit 1
 fi
+
 
 # set up testing in grading area
 cd grading-area
 
 CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
-javac -cp $CPATH *.java > compile-message.txt
+javac -cp $CPATH *.java > compile-message.txt 2>&1
 
 if [[ $? -ne 0 ]]
 then
-    echo "Compile error"
-    cat test-results.txt
+    echo "Compile error:"
+    cat compile-message.txt
+    echo "Your code failed to compile. This could be due to syntax errors, or when your implementation does not match the instructions."
     exit 1
 fi
 
-java -cp $CPATH org.junit.runner.JUnitCore Test$2 > test-results.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-results.txt
 
 # calculate score based on failures
 
-lastline = $(cat test-results.txt | tail -n 2 | head -n 1)
-tests = $(echo $lastline | awk -F'[, ]' '{print $3}')
-failures = $(echo $lastline | awk -F'[, ]' '{print $6}')
-successes = $(tests-failures)
+lastline=$(cat test-results.txt | tail -n 2|head -n 1)
+# cat test-results.txt | tail -n 2 | head -n 1 > tests-passed.txt
+echo $lastline
 
-echo "Your score is $successes / $tests"
+
+allpassed=$(echo $lastline | grep 'OK')
+
+# if there are no failues, the number of tests needs to be extracted another way
+# if [[ ! $tests =~ '^-?[0-9]+$' ]] || [[ ! $failures =~ '^-?[0-9]+$' ]]
+if [[ -n $allpassed ]]
+then
+   # tests=
+   echo "Congratulations, you passed all $(echo $lastline | grep -Eo '[0-9]+') tests!"
+   exit 0
+fi
+
+tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+# echo $(cat tests-passed.txt | head -n 1 | awk -F'[, ]' '{print $3}')
+
+failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+# echo $(cat tests-passed.txt | head -n 1 | awk -F'[ ]' '{print $6}')
+
+successes=$(($tests-$failures))
+
+echo "Your score is $successes/$tests."
+
+
+
 
